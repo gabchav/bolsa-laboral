6830441bdc3faf016c289ccb7281bcf4d703523d
Gabriel <gabriel@live.cl>
2022-07-10 08:46:31 -0400

    upload

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :ofertas do
     collection do
       get "search"
+      get "postuladas"
     end
     resources :empresa, only: [:new, :create]
   end  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
